0c03bb7bbfbc68fa6980af10dfdf3338e8b4c52d
dicastrom <51303820+dicastrom@users.noreply.github.com>
2020-05-19 17:25:03 -0400

    Update ClanInfo.py
    This Allows users to search for and find info on any clan!

--- ClanInfo.py
+++ ClanInfo.py
@@ -1,12 +1,7 @@
 import requests
-
-
 headers={
     'Accept':'application/json',
     'authorization':'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImQ3YTRiZTJlLTBhMzAtNDJhNC1hZjMwLTlmZTMyODgxMTZiYiIsImlhdCI6MTU4OTkwNDQ2NSwic3ViIjoiZGV2ZWxvcGVyL2MxNzVjMmE1LWZmNzMtOGExZS1mOWUyLWM1YjdhMDg4NThiOCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjk2LjU4LjE2Ny4xNTkiXSwidHlwZSI6ImNsaWVudCJ9XX0.VkrUEIejazU0v5mbA-8kO44bv5b0U79NQVU7piFj0Od7wXOBn2pOmHE_tXidHWCc_fAusf-F4G5zhrUxT81QoQ'
-
-
-
 }
 
 def get_user(tag):
@@ -22,13 +17,13 @@ def get_user(tag):
     print("\n")
 
 
-def show_clan():
+def show_clan(clan_tag):
     # return user's profile info
-    response = requests.get('https://api.clashofclans.com/v1/clans/%23P02CUUUU', headers=headers)
+    response = requests.get('https://api.clashofclans.com/v1/clans/%23'+clan_tag, headers=headers)
     clan_json = response.json()
     #print(clan_json)
     print("name = ", str(clan_json['name']))
-    print(clan_json['memberList'])
+    #print(clan_json['memberList'])
     memberList=clan_json['memberList']
     for member in memberList:
         print("_________________________________________")
@@ -43,8 +38,8 @@ def show_clan():
         print("Current Trophies =", member['trophies'])
         print("TOTAL donations (in AND out)= ", str(member['donations']+member['donationsReceived']))
         print("_________________________________________")
-
-show_clan()
+clan_tag=answer=input("Enter the Clan Tag (Ignore the # ): ")
+show_clan(clan_tag)
 answer=input("Do you want to search for a individual user? Y/N: ")
 while answer != 'Y' and answer !='N':
     print("Invalid option try again")
